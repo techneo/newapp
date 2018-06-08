dcdd94642aeee31f76ce05ed0d02234f768b4309
Suren <p.surendrakumar@gmail.com>
2018-06-08 20:43:49 +0530

    progress bar

--- src/widgets/dashboard.ts
+++ src/widgets/dashboard.ts
@@ -1,5 +1,8 @@
 import { WidgetBase } from '@dojo/widget-core/WidgetBase';
-import { v } from '@dojo/widget-core/d';
+import { v , w } from '@dojo/widget-core/d';
+import  Progress  from '@dojo/widgets/progress';
+// import WebAnimation from '@dojo/widget-core/meta/WebAnimation';
+
 // we will apply a theme mixin to the Worker widget.
 // A mixin is not intended to be used on its own, but instead works with a class to add useful functionality.
 import { theme, ThemedMixin } from '@dojo/widget-core/mixins/Themed';
@@ -12,6 +15,8 @@ var _name : number = 0;
 @theme(css)
 export default class Dashboard extends ThemedMixin(DashboardBase){
 
+
+
 	private _onClick() {
 		console.log('clicked');
 		_name = _name +  1;
@@ -19,8 +24,10 @@ export default class Dashboard extends ThemedMixin(DashboardBase){
 		this.invalidate();
 	}
 	protected render() {
+		
 		// register the click by calling 
-		return v('div', {} ,[ 
+		return v('div', { key:'root'} ,[ 
+			w( Progress , {  value : _name } ),
 			v('h1',{ 
 				classes: this.theme(css.h1) , onclick: this._onClick 
 			} ,['Clicked ' , _name.toString()])
